43e04799b6e55b66114f501cdca576a961837fb3
Shahin Dris <shahin@Haobeis-MacBook-Air.local>
2023-01-22 04:33:40 -0500

    conditional rendering feature

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,22 +4,40 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+import { Form, useFormik } from "formik";
+
 import Box from '@mui/material/Box';
 import { Button } from '@mui/material';
+import Fade from '@mui/material/Fade';
 import OutlinedCard from './cardComp'
 // import SendIcon from '@mui/icons-material/Send';
 import TextField from '@mui/material/TextField';
 import send_icon from './assets/sent.png'
 import { useState } from 'react';
-import Fade from '@mui/material/Fade';
 
 function App() {
   const [json,setJson] = useState({})
   const [submit, setSubmit] = useState(false);
-
-
- 
+  const formik = useFormik({
+    initialValues: {
+      p: '',
+    },
+    onSubmit: (values) => {
+      fetch("http://localhost:5000/", {
+        method: "POST",
+        body: JSON.stringify({topic : values.p}),
+        headers: { "Content-Type": "application/json" },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+        });
+    },
+  });
   return (
+  
+   
+    
     <div className="App">
     
       <header className="App-header">
@@ -28,15 +46,38 @@ function App() {
       <div className='main-container'>
         <h1 id="controversy-title">Co:ntroversy</h1>
         <h3 id="sub-title">Access all sides of the argument, delivered by AI</h3>
-        <Fade in={submit === false} timeout={2000}>
-        <form className="topic-form" action="/action_page.php">
-        <Box>
-        <TextField id="outlined-basic"  variant="standard" sx={{bgcolor:"white", mx:3, width: '20vw'}} />   
-        <Button variant="contained" size="small"  onClick={()=>setSubmit(true)} disabled={!setSubmit}> submit 
+         {/* <Fade in={submit === false} timeout={2000}>
+         <form className="topic-form" action="/action_page.php">
+         <Box>
+         <TextField id="outlined-basic"  variant="standard" sx={{bgcolor:"white", mx:3, width: '20vw'}} />   
+         <Button variant="contained" size="small"  onClick={()=>setSubmit(true)} disabled={!setSubmit}> submit 
+         </Button>
+         </Box>
+         </form> 
+         </Fade> */}
+        
+        {!submit &&<Box>
+        {/* <Form onSubmit={formik.handleSubmit}>
+        <TextField id="outlined-basic"  name="prompt"  type="prompt" variant="standard" sx={{bgcolor:"white", mx:3, width: '20vw'}} onChange={formik.handleChange}/>   
+        <Button variant="contained" size="small"  onClick={()=>{setSubmit(true); 
+                  }} disabled={!setSubmit} type="submit"> submit 
         </Button>
-        </Box>
-        </form> 
-        </Fade>
+        </Form> */}
+      
+      <form onSubmit={formik.handleSubmit}>
+       <input
+         id="p"
+         name="p"
+         type="text"
+         onChange={formik.handleChange}
+         value={formik.values.p}
+       />
+       <button type="submit">Submit</button>
+       </form>
+        </Box> }
+         
+        
+       
       </div>
       
       <Box sx = {{display: 'flex', mt : '5vh', justifyContent: 'center', alignContent: 'center'}}>
@@ -45,7 +86,8 @@ function App() {
           <OutlinedCard></OutlinedCard>
         </Box>
     </div>
-  );
+
+  )
 }
 
 export default App;

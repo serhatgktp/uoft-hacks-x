dc280faa8709d3411f6f97c22e744f73409421d2
Shahin Dris <shahin@Haobeis-MacBook-Air.local>
2023-01-22 01:20:28 -0500

    merged

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
 import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import Box from '@mui/material/Box';
+import { Button } from '@mui/material';
+// import SendIcon from '@mui/icons-material/Send';
+import TextField from '@mui/material/TextField';
 import send_icon from './assets/sent.png'
+import { useState } from 'react';
 
 function App() {
+  const [json,setJson] = useState({})
+  const [submit, setSubmit] = useState(false);
   return (
     <div className="App">
       <header className="App-header">
@@ -10,11 +22,13 @@ function App() {
       <div className='main-container'>
         <h1 id="controversy-title">Co:ntroversy</h1>
         <h3 id="sub-title">Access all sides of the argument, delivered by AI</h3>
-        <div className = "form-container">
-          <form className="topic-form" action="/action_page.php">
-            <input type="text" id="topic-textbox" name="topic" />
-          </form> 
-        </div>
+        <form className="topic-form" action="/action_page.php">
+        
+       
+        <TextField id="outlined-basic"  variant="standard" sx={{bgcolor:"white", mx:3}} />
+        <Button variant="contained" size="small" > submit </Button>
+        
+        </form> 
       </div>
     </div>
   );

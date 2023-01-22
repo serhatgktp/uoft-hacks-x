45352d72226485dc939e07d54d6b8fcd88d35d41
Efkan S. Goktepe <76130958+serhatgktp@users.noreply.github.com>
2023-01-22 01:30:10 -0500

    Update event_bus.py

--- backend/event_bus.py
+++ backend/event_bus.py
@@ -4,10 +4,6 @@ def get_conversation(topic):
     ggr.init_open()
     rw_perspective = ggr.get_gpt_response(topic)
     challenger_perspective = ggr.get_gpt_response(rw_perspective, mode='disagree')
-    neutral_perspective = ggr.get_gpt_response(rw_perspective, mode='neutral')
-    print('- - - - - - - - -')
-    print("Right-wing perspective:", rw_perspective)
-    print('- - - - - - - - -')
-    print("Left-wing perspective:", challenger_perspective)
-    print('- - - - - - - - -')
-    print("Neutral perspective:", neutral_perspective)
+    neutral_perspective = ggr.get_gpt_response(topic, mode='neutral')
+    
+    return rw_perspective, challenger_perspective, neutral_perspective

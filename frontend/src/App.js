fa9200a9c0c20f47c24867fa5b59e53038c68488
Pedram Meskoub <63091219+pedrammeskoub@users.noreply.github.com>
2023-01-22 01:45:06 -0500

    Add card comp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import Box from '@mui/material/Box';
 import { Button } from '@mui/material';
 // import SendIcon from '@mui/icons-material/Send';
 import TextField from '@mui/material/TextField';
+import OutlinedCard from './cardComp'
 import send_icon from './assets/sent.png'
 import { useState } from 'react';
 
@@ -17,18 +18,20 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p id='header-title'>controversy.io</p>
+        <p id='header-title'>Controversy.io</p>
       </header>
       <div className='main-container'>
         <h1 id="controversy-title">Co:ntroversy</h1>
         <h3 id="sub-title">Access all sides of the argument, delivered by AI</h3>
         <form className="topic-form" action="/action_page.php">
-        
-       
-        <TextField id="outlined-basic"  variant="standard" sx={{bgcolor:"white", mx:3}} />
+        <TextField id="outlined-basic"  variant="standard" sx={{bgcolor:"white", mx:3, width: '20vw'}} />
         <Button variant="contained" size="small" > submit </Button>
-        
         </form> 
+        <Box sx = {{display: 'flex', mt : '5vh', justifyContent: 'center', alignContent: 'center'}}>
+          <OutlinedCard></OutlinedCard>
+          <OutlinedCard></OutlinedCard>
+          <OutlinedCard></OutlinedCard>
+        </Box>
       </div>
     </div>
   );

9e57a0bf4f555dad1d140d7d82f27244508ff415
Shahin Dris <shahin@Haobeis-MacBook-Air.local>
2023-01-21 22:43:17 -0500

    basic layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import {Box, Typography}  from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Box display={"flex"} alignItems={"center"} justifyContent={"center"} sx={{height:"100vh"}}>
+    <Box width= {"900px"} display={"flex"} flexDirection={"column"} justifyContent={"space-between"}>
+      <Typography variant='h1' component="h1"> Controvesy.io</Typography>
+   </Box>
+   </Box>
   );
 }
 

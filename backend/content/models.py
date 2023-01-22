ccf30133a8c3f5478b4bfb168a2780188b99ea0e
Pedram Meskoub <63091219+pedrammeskoub@users.noreply.github.com>
2023-01-21 20:01:09 -0500

    Frontend changes push

--- backend/content/models.py
+++ backend/content/models.py
@@ -2,16 +2,15 @@ from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
-class ArticleModel(models.Model):
+class PodcastModel(models.Model):
     title = models.CharField(max_length = 500, null = True)
-    date = models.CharField(max_length = 20, null=True)
-    session =  models.CharField(max_length = 20, null = True)
-    first = models.TextField(null = True)
-    second = models.TextField(null = True)
-    third = models.TextField(null = True)
-    fourth = models.TextField(null = True)
-    url = models.URLField(null = True)
-    photo = models.ImageField(upload_to='pics', null = True)
+    description =  models.CharField(max_length = 20, null = True)
+    photo_url = models.CharField(max_length = 30, null = True)
+    category = models.JSONField()
+    
+
+class EpisodeModel(PodcastModel):
+    summary = models.CharField(max_length = 1000, null = True)
 
 
 

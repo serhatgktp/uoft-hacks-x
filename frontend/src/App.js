a68de9ca6cb36607a98e023ea4cb9fe02339266d
Pedram Meskoub <63091219+pedrammeskoub@users.noreply.github.com>
2023-01-22 00:55:07 -0500

    style changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,12 +8,13 @@ function App() {
         <p id='header-title'>controversy.io</p>
       </header>
       <div className='main-container'>
-        <h1 id="controversy-title">co:ntroversy</h1>
+        <h1 id="controversy-title">Co:ntroversy</h1>
         <h3 id="sub-title">Access all sides of the argument, delivered by AI</h3>
-        <form className="topic-form" action="/action_page.php">
-          <input type="text" id="topic-textbox" name="topic" />
-          <img id="send_icon" alt="send-icon" src={send_icon} target=""/>
-        </form> 
+        <div className = "form-container">
+          <form className="topic-form" action="/action_page.php">
+            <input type="text" id="topic-textbox" name="topic" />
+          </form> 
+        </div>
       </div>
     </div>
   );

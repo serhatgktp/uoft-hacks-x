dcdfd446652d24c2eba096dc0964c1be06cf3d97
Pedram Meskoub <63091219+pedrammeskoub@users.noreply.github.com>
2023-01-22 04:16:03 -0500

    merge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,37 +11,39 @@ import OutlinedCard from './cardComp'
 import TextField from '@mui/material/TextField';
 import send_icon from './assets/sent.png'
 import { useState } from 'react';
+import Fade from '@mui/material/Fade';
 
 function App() {
   const [json,setJson] = useState({})
   const [submit, setSubmit] = useState(false);
+
+
  
   return (
     <div className="App">
-       <Box >
+    
       <header className="App-header">
         <p id='header-title'>Controversy.io</p>
       </header>
       <div className='main-container'>
         <h1 id="controversy-title">Co:ntroversy</h1>
         <h3 id="sub-title">Access all sides of the argument, delivered by AI</h3>
+        <Fade in={submit === false} timeout={2000}>
         <form className="topic-form" action="/action_page.php">
-        {!submit &&<Box>
+        <Box>
         <TextField id="outlined-basic"  variant="standard" sx={{bgcolor:"white", mx:3, width: '20vw'}} />   
         <Button variant="contained" size="small"  onClick={()=>setSubmit(true)} disabled={!setSubmit}> submit 
         </Button>
-        </Box>}
+        </Box>
         </form> 
-        
-       
+        </Fade>
       </div>
-      </Box>
+      
       <Box sx = {{display: 'flex', mt : '5vh', justifyContent: 'center', alignContent: 'center'}}>
           <OutlinedCard></OutlinedCard>
           <OutlinedCard></OutlinedCard>
           <OutlinedCard></OutlinedCard>
         </Box>
-        
     </div>
   );
 }
